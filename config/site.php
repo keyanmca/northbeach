b8b78ae691d5f5985b98cafd81e510f95b0d4b4b
Sarah Pedley <sarahpedley@hamdigital.com.au>
2013-09-05 11:38:30 +0800

    New site upload all.

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'nbcc');
+define('PASSWORD_SALT', 'y8IJUzafirTuoP5Wl5AWw53yBMteohJrG6UCCZfIzlMYvi9D8WKqTSl8CKLfnJ1K');
---
+++ themes/NBCC - HAM Digital/elements/header.php
@@ -0,0 +1,43 @@
+<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+<?php  Loader::element('header_required'); ?>
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('main.css')?>" />
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getThemePath() ?>/css/reset.css" />
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getThemePath() ?>/css/jquery.fancybox-1.3.4.css" />
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getThemePath() ?>/css/jqtransform.css" />
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getThemePath() ?>/css/grid.css" />
+
+<link href='http://fonts.googleapis.com/css?family=Alfa+Slab+One' rel='stylesheet'>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/scripts.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/jquery.color.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/jquery.backgroundpos.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/jquery.easing.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/jquery.mousewheel.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/jquery.fancybox-1.3.4.pack.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/googleMap.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/superfish.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/switcher.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/bgStretch.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/sImg.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/forms.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/MathUtils.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/jquery.cycle.all.min.js' ?>"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath() . '/js/jquery.jqtransform.js' ?>"></script>
+
+    <!--[if lt IE 9]>
+  	<script src="js/html5.js"></script>
+    <link rel="stylesheet" href="css/ie.css" media="all">
+    <![endif]-->
+	<!--[if lt IE 8]>
+		<div style=' clear: both; text-align:center; position: relative;'>
+			<a href="//windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a>
+		</div>
+	<![endif]-->
+	
+	
+
+</head>
+<body> 
